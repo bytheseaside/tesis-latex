9247b71d21bf300eb7c07d3ae25f6df74c78e1d5
Brisa Rojas <rojaasbrisa@gmail.com>
2025-11-16 12:52:39 +0100

    Update references and improve PDF build script

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 latexmk -c -cd 00_Main/Main.tex
-latexmk -pdf -shell-escape -cd -interaction=nonstopmode 00_Main/Main.tex
+latexmk -f -pdf -shell-escape -cd -interaction=nonstopmode 00_Main/Main.tex
