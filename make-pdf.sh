2a0544c0dfce6d22da69ca4fefb0e47f799f54ba
Luciana Diaz Kralj <ldiazkralj@chyps.com.ar>
2026-01-30 06:51:50 -0300

    Added compilation script for glossary

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 latexmk -c -cd 00_Main/Main.tex
-latexmk -f -pdf -shell-escape -cd -interaction=nonstopmode -bibtex 00_Main/Main.tex
+# latexmk -f -pdf -shell-escape -cd -interaction=nonstopmode -bibtex 00_Main/Main.tex
+latexmk -f -pdf -shell-escape -cd -interaction=nonstopmode -bibtex -e '$makeglossaries = "makeglossaries %O %S";' -e 'add_cus_dep("glo", "gls", 0, "run_makeglossaries");' -e 'add_cus_dep("acn", "acr", 0, "run_makeglossaries");' -e 'sub run_makeglossaries { system("makeglossaries $_[0]"); }' 00_Main/Main.tex
